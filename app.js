939fab026256d16aaf0ec6b52814323724ba31b2
seqi <tomseqi@live.co.uk>
2017-12-24 14:27:04 +0000

    Added metadata to log

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ let logger = require('./logger')
 let interval = 1000 * 2
 
 log = () => {
-    logger.debug('Tick')
+    let  i = 0;
+    logger.debug('Tick', 
+        { 
+            'someMeta': 'someData',
+            'bigol': 'doinks',
+            'array': [ 1, 2, 3 ],
+            'counter': ++i
+        })
 }
 
 setInterval(log, interval)
